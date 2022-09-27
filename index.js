68aab7efd7ae0f2da58021a67376116d5bcd0f79
Luigi Enrico <luigibongiolo@gmail.com>
2022-09-26 23:04:43 -0300

    teste

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 
-window.onload = function loadFiles(){
-    importFiles();
-}
-function importFiles(){
-
-    let header = document.getElementById('header');
-    header.innerHTML = `<nav class="font-size navbar navbar-expand-lg bg-light" >
+window.onload = function loadFiles() {
+  
+  let header = document.getElementById('header');
+  header.innerHTML = `<nav class="font-size navbar navbar-expand-lg bg-light" >
     <div class="container-fluid  navbar-dark bg-dark">
       <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
         <div class="navbar-nav space-between" id="header">
@@ -18,20 +15,12 @@ function importFiles(){
       </div>
     </div>
   </nav>`;
-    // console.log(header)
-    document.getElementById('import').innerHTML = `</div> <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+  // console.log(header)
+  document.getElementById('import').innerHTML = `</div> <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-  <link href="style.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8"
-    crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
-    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
-    crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
-    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz"
-    crossorigin="anonymous"></script>`;
-    document.getElementById('footer').innerHTML = `<!-- Footer -->
+  <link href="style.css" rel="stylesheet">`;
+  let footer = document.getElementById('footer');
+  footer.innerHTML = `<!-- Footer -->
     <footer class="bg-dark text-center text-white">
       <!-- Grid container -->
       <div class="container p-4">
